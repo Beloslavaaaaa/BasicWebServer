e22bf92b77a3b9f33e096b3bd6f10b6676f4bee3
Beloslavaaaaa <b.marinova@yandex.com>
2025-02-14 16:43:43 +0200

    second commit

---
+++ BasicWebServer.Server/HttpServer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWebServer.Server
+{
+    public class HttpServer
+    {
+        private readonly IPAddress ipAdress;
+        private readonly int port;
+        private readonly TcpListener serverListener;
+
+        public HttpServer(string ipAdress, int port)
+        {
+            this.ipAdress = IPAddress.Parse(ipAdress);
+            this.port = port;
+            this.serverListener = new TcpListener(this.ipAdress, port);
+        }
+        public void Start()
+        {
+            this.serverListener.Start();
+            while(true)
+            {
+                var connection = serverListener.AcceptTcpClient();
+                var networkStream = connection.GetStream();
+                var requestText = this.ReadRequest(networkStream);
+                Console.WriteLine(requestText);
+                WriteResponse(networkStream, "Hello from the server!");
+                connection.Close();
+
+            }
+        }
+        public void WriteResponse(NetworkStream networkStream, string message)
+        {
+            var contentLenght = Encoding.UTF8.GetByteCount(message);
+            var response = $@"HTTP/1.1 200 OK
+Content-Type: text/plain; charset=UTF-8
+Content-Length: {contentLenght}
+{message}";
+            var responseBytes = Encoding.UTF8.GetBytes(response);
+            networkStream.Write(responseBytes);
+        }
+        private string ReadRequest(NetworkStream  networkStream)
+        {
+            var bufferLenght = 1024;
+            var buffer = new byte[bufferLenght];
+            var totalBytes = 0;
+            var requestBuilder = new StringBuilder();
+
+            do
+            {
+                var bytesRead = networkStream.Read(buffer, 0, bufferLenght);
+                totalBytes += bytesRead;
+                if(totalBytes > 10 * 1024)
+                {
+                    throw new InvalidOperationException("Request is too large");
+                }
+                requestBuilder.Append(Encoding.UTF8.GetString(buffer, 0, bytesRead));
+            } while (networkStream.DataAvailable);
+            return requestBuilder.ToString();   
+        }
+    }
+}
--- BasicWebServer/Program.cs
+++ BasicWebServer/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace BasicWebServer
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace BasicWebServer
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            
+            var ipAdress = IPAddress.Parse("127.0.0.1");
+            var port = 8080;
+            var serverListener = new TcpListener(ipAdress, port);
+            serverListener.Start();
+
+            var connection = serverListener.AcceptTcpClient();
+            var networkStream = connection.GetStream();
+            var content = "Hellofrom the server";
+            var contentLenght = Encoding.UTF8.GetByteCount(content);
+
+            var response = $@"HTTP/1.1 200 OK
+Content-Type: text/plain; charset=UTF-8
+Content-Length: {contentLenght}
+{content}";
+            var responseBytes = Encoding.UTF8.GetBytes(response);
+            networkStream.Write(responseBytes);
+            connection.Close();
         }
     }
 }
